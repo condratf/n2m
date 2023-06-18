baf66f9a7b18a6fbe3dec1fa448935a9ac5ae52c
f.kondratov <f.kondratov@idynsys.org>
2023-06-18 20:36:46 +0200

    improvments

--- src/app/policies/PrivacyPolicy/page.tsx
+++ src/app/policies/PrivacyPolicy/page.tsx
@@ -1,29 +1,29 @@
-import {FC} from 'react'
+import { FC } from 'react'
 // local
 import { PolicyPage } from '@/components/policies'
 
-export const PrivacyPolicy: FC = () => {
-  const policiesList = [
-    { title: 'Overview', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
-    {
-      title: 'Acceptance of this policy', ul: [
-        'Lorem ipsum dolo',
-        'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nis',
-        'Uis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-        'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-      ]
-    },
-    {
-      title: 'Collection of information by third party services', ol: [
-        'Lorem ipsum dolo',
-        'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nis',
-        'Uis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-        'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-      ]
-    },
-    { title: 'Use of collected information', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
-  ]
+const policiesList = [
+  { title: 'Overview', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+  {
+    title: 'Acceptance of this policy', ul: [
+      'Lorem ipsum dolo',
+      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nis',
+      'Uis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+      'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    ]
+  },
+  {
+    title: 'Collection of information by third party services', ol: [
+      'Lorem ipsum dolo',
+      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nis',
+      'Uis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+      'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    ]
+  },
+  { title: 'Use of collected information', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+]
 
+const PrivacyPolicy: FC = () => {
   return (
     <PolicyPage title="Privacy Policy" policiesList={policiesList} />
   )
--- src/app/policies/TermsOfUse/page.tsx
+++ src/app/policies/TermsOfUse/page.tsx
@@ -2,28 +2,28 @@ import { FC } from 'react'
 // local
 import { PolicyPage } from '@/components/policies'
 
-export const TermsOfUse: FC = () => {
-  const policiesList = [
-    { title: 'Overview', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
-    {
-      title: 'Acceptance of this policy', ul: [
-        'Lorem ipsum dolo',
-        'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nis',
-        'Uis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-        'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-      ]
-    },
-    {
-      title: 'Collection of information by third party services', ol: [
-        'Lorem ipsum dolo',
-        'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nis',
-        'Uis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-        'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-      ]
-    },
-    { title: 'Use of collected information', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
-  ]
+const policiesList = [
+  { title: 'Overview', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+  {
+    title: 'Acceptance of this policy', ul: [
+      'Lorem ipsum dolo',
+      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nis',
+      'Uis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+      'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    ]
+  },
+  {
+    title: 'Collection of information by third party services', ol: [
+      'Lorem ipsum dolo',
+      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nis',
+      'Uis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+      'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    ]
+  },
+  { title: 'Use of collected information', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+]
 
+const TermsOfUse: FC = () => {
   return (
     <PolicyPage title="Terms of Use" policiesList={policiesList} />
   )
