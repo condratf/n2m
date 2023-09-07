9fa4696898dbfbf66c314c2e758e502fe1b3abf7
f.kondratov <f.kondratov@idynsys.org>
2023-09-07 10:31:30 +0200

    DEV-11343 change address

--- src/components/layout/Footer/resources.ts
+++ src/components/layout/Footer/resources.ts
@@ -16,9 +16,9 @@ export const policiesLinks = [
   { name: 'Commissions and fees', href: routes.policies.commissionsAndFees },
 ]
 export const licensesLinks = [
-  { name: 'RM 8, S-V 6/F, VALIANT INDUSTRIAL CENTRE 2-12 AU PUI WAN ST FO TAN NT, HONG KONG', href: '' },
-  { name: 'UNI: Unit 04, 7/f, Bright way, Tower, no. 33, Mong kok road, Kowloon, Hong Kong', href: '' },
-  { name: 'Company number: 1205540', href: '' },
+  // { name: 'RM 8, S-V 6/F, VALIANT INDUSTRIAL CENTRE 2-12 AU PUI WAN ST FO TAN NT, HONG KONG', href: '' },
+  { name: 'Unit D01, 3/F., Wong King Industrial Building,No.2 Tai Yau Street, Kowloon, Hong Kong', href: '' },
+  { name: 'Company number: 2396895', href: '' },
   //{ name: '29 June 2016, MS Ada L L CHUNG, Registrar of Companies, Hong Kong Special Administrative Region', href: '' },
   { name: '2016-2023 © DANIER LTD. DANIER LIMITED', href: '' },
 ]
